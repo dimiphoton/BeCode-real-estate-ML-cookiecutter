7fbd0222ec6a46da6c975798c4159db66e6df664
Dimitri MARCHAND <pro.dimitrimarchand@gmail.com>
2023-01-17 15:07:11 +0100

    save

--- data/raw/cleaning_utils.py
+++ data/raw/cleaning_utils.py
@@ -9,7 +9,7 @@ rent_cols=['Living Area', 'Number of facades',
        'Terrace','type','zipcode']
 
 def x1000(df_sale):
-    if (df_sale['Price']<50000) & (df_sale['Living Area']>100):
+    if (df_sale['Price']<40000) & (df_sale['Living Area']>80):
         return 1000*df_sale['Price']
 
 def step0():
@@ -24,20 +24,56 @@ def step0():
 
     for col in ['Area of the garden','Area of the terrace','Surface area of the plot of land','Surface of the land']:
         df0[col].fillna(value=0,inplace=True)
+    df0.dropna
     
     df_sale=df0.loc[df0['To sell']==True][sale_cols]
     df_rent=df0.loc[df0['To rent']==True][rent_cols]
-    return df_sale, df_rent
+    return df_sale.dropna(), df_rent.dropna()
 
-def step1(df_sale):
+def step1bis(df_sale):
     """
 
     Args:
         df (_type_): _description_
     """
+    df_sale['Cleaned Price']= 0
     for index, row in df_sale.iterrows():
         if (row['Price']<50000) & (row['Living Area']>100):
             #print('anormal')
             row['Cleaned Price']=1000*row['Price']
+
         else:
             row['Cleaned Price']=row['Price']
+    return df_sale
+
+def step1(df):
+    df["Price"] = df["Cleaned Price"].apply(lambda x: x*1000 if x < 1000 else x)
+    return df
+
+def postcode2province(postcode):
+    if postcode<1300:
+        return 'Brussels'
+    elif postcode<1500:
+        return 'Walloon Brabant'
+    elif postcode<2000:
+        return 'FLemish Brabant'
+    elif postcode<3000:
+        return 'Antwerp'
+    elif postcode<3500:
+        return 'FLemish Brabant'
+    elif postcode<4000:
+        return 'Limburg'
+    elif postcode<5000:
+        return 'Liege'
+    elif postcode<5681:
+        return 'Namur'
+    elif postcode<6600:
+        return 'Hainaut'
+    elif postcode<7000:
+        return 'Luxemburg'
+    elif postcode<8000:
+        return 'Hainaut'
+    elif postcode<9000:
+        return 'West Flanders'
+    elif postcode<10000:
+        return 'East Flanders'
